be828cbf5f99ea9ac7c2820c89267a6099f8ff9d
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2026-05-30 23:30:50 +0530

    Add EvenOrOdd program to check number parity

---
+++ JAVA CORE/EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        
+        //even
+        if(number % 2 == 0){
+            System.out.println(number + " is an even number");
+        }else{ //odd
+            System.out.println(number + " is an odd number");
+        }
+
+        sc.close();
+    }
+    
+}
